e8e495d965a8820a8eb09189f18c0832b95df0ed
noo6sploit <itzz.noo6.games@gmail.com>
2026-06-06 08:55:23 -0400

    Create full_script.lua

---
+++ full_script.lua
@@ -0,0 +1,4 @@
+-- auto rebirth + autoclicker OPEN SOURCE
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noo6sploit/phonk-clicker-script/main/script2.lua"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noo6sploit/phonk-clicker-script/refs/heads/main/script.lua"))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noo6sploit/phonk-clicker-script/refs/heads/main/mult.lua"))() 
