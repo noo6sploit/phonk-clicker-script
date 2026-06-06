faa08af23180460821cfae9c8c66527924a90568
noo6sploit <itzz.noo6.games@gmail.com>
2026-06-06 07:22:51 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,4 +2,9 @@ while true do wait(1)
 local Event = game:GetService("ReplicatedStorage").Remotes.ClickBrainrot
 Event:FireServer(
     111111111
-) end  -- noo6 is cool _noo6.termed on discord 
+) end -- auto money
+then
+while true do wait(1)
+local Event = game:GetService("ReplicatedStorage").Remotes.Rebirth
+Event:InvokeServer() end -- auto rebirth 
+end
