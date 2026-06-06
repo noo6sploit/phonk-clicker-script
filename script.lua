5876e1539a30763fab24f33136048a631d1d0ee6
noo6sploit <itzz.noo6.games@gmail.com>
2026-06-06 07:55:36 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,12 +1,10 @@
 
-
+------------------------
 while true do wait(0.5)
 local RS = game:GetService("ReplicatedStorage")
 
 
 local Remotes = RS:FindFirstChild("Remotes")
-
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/noo6sploit/phonk-clicker-script/refs/heads/main/script2.lua"))()   
 
 local ClickBrainrot = Remotes and Remotes:FindFirstChild("ClickBrainrot")
 
@@ -16,3 +14,5 @@ while true do
 end 
 end
 -- noo6 was here
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noo6sploit/phonk-clicker-script/refs/heads/main/script2.lua"))()    
+end
